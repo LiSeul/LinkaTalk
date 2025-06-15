5c1ee28fec926d4fd79ce8f10acb4bc37e054059
liseul <seul_1224@naver.com>
2025-06-15 19:46:54 +0900

    chore: Project Setting

---
+++ LinkaTalk/LinkaTalk/LinkaTalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkaTalkApp.swift
+//  LinkaTalk
+//
+//  Created by Seulki Lee on 6/15/25.
+//
+
+import SwiftUI
+
+@main
+struct LinkaTalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
